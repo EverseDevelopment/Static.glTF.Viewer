99783d3f84c2224add0fd4bcc371b6735d90f77d
vnoves <vnoves@e-verse.com>
2024-02-16 21:28:52 -0500

    Cleanupfile

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,31 +5,6 @@ import { REVISION } from 'three';
 
 export function Footer() {
 	return (
-		// <footer>
-		// 	<a class="item" target="_blank" href="https://threejs.org/">
-		// 		three.js r{REVISION}
-		// 	</a>
-
-		// 	<span class="separator" aria-hidden="true">
-		// 		|
-		// 	</span>
-
-		// 	<a
-		// 		class="item"
-		// 		target="_blank"
-		// 		href="https://github.com/donmccurdy/three-gltf-viewer/issues/new"
-		// 	>
-		// 		help & feedback
-		// 	</a>
-
-		// 	<span class="separator" aria-hidden="true">
-		// 		|
-		// 	</span>
-
-		// 	<a class="item" target="_blank" href="https://github.com/donmccurdy/three-gltf-viewer">
-		// 		github
-		// 	</a>
-		// </footer>
 		<footer>
 			<a href="http://e-verse.com">
 				<img src="EverseBlackLogo.png" alt="e-verse logo" class="header-image">
--- src/environments.js
+++ src/environments.js
@@ -9,16 +9,4 @@ export const environments = [
 		name: 'Neutral',
 		path: null,
 	},
-	{
-		id: 'venice-sunset',
-		name: 'Venice Sunset',
-		path: 'https://storage.googleapis.com/donmccurdy-static/venice_sunset_1k.exr',
-		format: '.exr',
-	},
-	{
-		id: 'footprint-court',
-		name: 'Footprint Court (HDR Labs)',
-		path: 'https://storage.googleapis.com/donmccurdy-static/footprint_court_2k.exr',
-		format: '.exr',
-	},
 ];
